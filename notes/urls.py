a368af88e46d34ba94d3aa061d3244fbd19a29e5
jahnavi <jahnavipatel0206@gmail.com>
2022-03-02 21:38:47 +0530

    one

--- notes/urls.py
+++ notes/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.listViews.as_view(),name="notes.list"),
+    path('', views.listViews.as_view(),name="notes.list"),
     path('notes/<int:pk>',views.notedetail.as_view(),name="notes.details"),
     path('notes/<int:pk>/edit',views.updatenote.as_view(),name="notes.update"),
     path('notes/<int:pk>/delete',views.deletenote.as_view(),name="notes.delete"),
